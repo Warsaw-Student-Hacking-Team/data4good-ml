2592067925f9bb9b0ee8ac407bb1c583c9b1dfa5
Aleksander Samek <olek.samek@gmail.com>
2024-04-20 19:44:05 +0200

    batchnorm

--- Models.py
+++ Models.py
@@ -5,12 +5,36 @@ import torch.optim as optim
 class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(MLP, self).__init__()
+        self.bn1 = nn.BatchNorm1d(input_size)
         self.fc1 = nn.Linear(input_size, hidden_size)
+        self.bn2 = nn.BatchNorm1d(hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, output_size)
         
     def forward(self, x):
+        x = self.bn1(x)
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.bn2(x)
         x = self.fc2(x)
+        return x
+class MLP_v2(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.bn1 = nn.BatchNorm1d(input_size)
+        self.bn2 = nn.BatchNorm1d(hidden_size)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        x = self.bn1(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.bn2(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.bn2(x)
+        x = self.fc3(x)
         return x
