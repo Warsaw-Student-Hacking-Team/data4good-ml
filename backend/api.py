8821cb1786a77f5d531fa3950dc34783617af9fd
Michał Oręziak <micorix.dev@gmail.com>
2024-04-21 02:11:11 +0200

    Fix api params

--- backend/api.py
+++ backend/api.py
@@ -6,6 +6,6 @@ api_router = APIRouter()
 
 
 @api_router.get("/predict_pm10")
-def get_ppm10_prediction_route(hour: int = 2):
-    prediction = predict_pm10(hour)
+def get_ppm10_prediction_route(l_spared: int = 1000):
+    prediction = predict_pm10(l_spared)
     return prediction.tolist()
